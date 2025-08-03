cabfbf9298250b9ac5916a7db6a07a5d25995a81
serelik <Zdanowitch.Iv@yandex.ru>
2025-08-04 00:44:09 +0700

    add dependencies

--- ui/feature/history/build.gradle.kts
+++ ui/feature/history/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinAndroidKsp)
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
@@ -36,8 +39,16 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.hilt.android)
+    implementation(libs.navigation.hilt)
+    ksp(libs.hilt.compiler)
 }
---
+++ ui/feature/history/src/main/java/com/serelik/surfdailyquiz/ui/feature/history/HistoryEmptyScreen.kt
@@ -0,0 +1,8 @@
+package com.serelik.surfdailyquiz.ui.feature.history
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryEmptyScreen() {
+
+}
